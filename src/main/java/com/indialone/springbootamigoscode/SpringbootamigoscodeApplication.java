db5f300a6649707757b7c4f152e56f7bb9a6270a
Natavar Prajapati <prajapatinatavar21197@gmail.com>
2021-12-28 00:04:20 +0530

    first commit

---
+++ src/main/java/com/indialone/springbootamigoscode/SpringbootamigoscodeApplication.java
@@ -0,0 +1,13 @@
+package com.indialone.springbootamigoscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootamigoscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootamigoscodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/indialone/springbootamigoscode/SpringbootamigoscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indialone.springbootamigoscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootamigoscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
