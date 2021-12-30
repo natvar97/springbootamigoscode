6a6c8ff9980699325122671e6760ddf2083b2911
Indialone <prajapatinatavar21197@gmail.com>
2021-12-30 23:49:18 +0530

    temp commit

--- src/main/java/com/indialone/springbootamigoscode/SpringbootamigoscodeApplication.java
+++ src/main/java/com/indialone/springbootamigoscode/SpringbootamigoscodeApplication.java
@@ -2,12 +2,16 @@ package com.indialone.springbootamigoscode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootamigoscodeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootamigoscodeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootamigoscodeApplication.class, args);
+    }
+
+
 
 }
